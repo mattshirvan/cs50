0a2642c446af9ed9f9f50a52245d5f3ec55397a5
mattshirvan <mattshirvan@users.noreply.github.com>
2019-11-10 21:39:01 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -2,28 +2,87 @@
 #include <stdio.h>
 
 int main(void)
-
 {
-	int n;
-	int c;
-	int j;
-	do
-	{
-		n = get_int("Height: ");
-	} 
-	while (n < 1 || n > 8);
-		c = n;
-
-	for (int i = 0; i < n; i++)
-	{		
-		for (j = 0; j <= n; j++)
-		{
-			if (j < c)
-				printf(" ");
-			else 
-				printf("#");						
-		}
-		c--;
-		printf("\n");
-	}	
+    int n;
+    
+    do 
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1 || n > 8);
+    
+   
+    
+    if (n == 1)
+    {
+        printf("#\n");
+    }
+            
+        
+    else if (n == 2)
+    {
+        printf(" "); printf("#\n");
+        printf("##\n");
+    }
+    
+    else if (n == 3)
+    {
+        printf(" "); printf(" "); printf("#\n");
+        printf(" "); printf("##\n");
+        printf("###\n");
+    }
+    
+    else if (n == 4)
+    {
+        printf(" "); printf(" "); printf(" "); printf(" #\n");
+        printf(" "); printf(" "); printf("##\n");
+        printf(" "); printf("###\n");
+        printf("####\n");
+    }
+    
+    else if (n == 5)
+    {
+        printf(" "); printf(" "); printf(" "); printf(" "); printf("#\n");
+        printf(" "); printf(" "); printf(" "); printf("##\n");
+        printf(" "); printf(" "); printf("###\n");
+        printf(" "); printf("####\n");
+        printf("#####\n");
+    }
+    
+    else if (n == 6)
+    {
+        printf(" "); printf(" "); printf(" "); printf(" "); printf(" "); printf("#\n");
+        printf(" "); printf(" "); printf(" "); printf(" "); printf("##\n");
+        printf(" "); printf(" "); printf(" "); printf("###\n");
+        printf(" "); printf(" "); printf("####\n");
+        printf(" "); printf("#####");
+        printf("######\n");
+    }
+    
+    else if (n == 7)
+    {
+        printf(" "); printf(" "); printf(" "); printf(" "); printf(" "); printf(" "); printf(" #\n");
+        printf(" "); printf(" "); printf(" "); printf(" "); printf(" "); printf("##\n");
+        printf(" "); printf(" "); printf(" "); printf(" "); printf("###\n");
+        printf(" "); printf(" "); printf(" "); printf("####\n");
+        printf(" "); printf(" "); printf("#####\n");
+        printf(" "); printf("######\n");
+        printf("#######\n");
+    }
+    
+    else if (n == 8)
+    {
+        printf(" "); printf(" "); printf(" "); printf(" "); printf(" "); printf(" "); printf(" "); printf("#\n");
+        printf(" "); printf(" "); printf(" "); printf(" "); printf(" "); printf(" "); printf("##\n");
+        printf(" "); printf(" "); printf(" "); printf(" "); printf(" "); printf("###\n");
+        printf(" "); printf(" "); printf(" "); printf(" "); printf("####\n");
+        printf(" "); printf(" "); printf(" "); printf("#####\n");
+        printf(" "); printf(" "); printf("######\n");
+        printf(" "); printf("#######\n");
+        printf("########\n");
+    }
+    
 }
+
+
+ 
