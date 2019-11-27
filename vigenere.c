92d0690c43cdd96fbf359e24543392602f868fa9
mattshirvan <mattshirvan@users.noreply.github.com>
2019-11-27 18:57:20 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- vigenere.c
+++ vigenere.c
@@ -11,31 +11,33 @@ int main(int argc, string argv[])
     // check if argc is only 2
     if (argc != 2)
     {
+        // print error
         printf("Usage: ./vigenere keyword\n");
         return 1;
     }
 
     // store key
     string k = argv[1];
-    int h = strlen(argv[1]);
-
+    int h = strlen(k);
+    
     // iterate over argc for arguments
-    for (int i = 0; i < argc; i++)
+    for (int i = 0; i < h ; i++)
     {
-        // validate command line is not alpha
+        // validate command line is alpha
         if (!isalpha(k[i]))
         {
+            // print error
             printf("Usage: ./vigenere keyword\n");
             return 1;
         }
     }
-    
+   
     // prompt user for string
     string s = get_string("plaintext: ");
     printf("ciphertext: ");
 
     // iterate over plain text
-    for (int i = 0, j = 0, n = strlen(s); i < n; i++)
+    for (int i = 0, n = 0, j = strlen(s); i < j; i++)
     {
         // keep uppercase
         if (isalpha(s[i]))
@@ -43,21 +45,21 @@ int main(int argc, string argv[])
             // cipher only alpha characters
             if (islower(s[i]))
             {
-                printf("%c", (s[i] - 'a' + toupper(k[j]) - 'A') % 26 + 'a');
+                printf("%c", (s[i] - 'a' + toupper(k[n]) - 'A') % 26 + 'a');
             }
             
             // keep uppercase
             else if (isupper(s[i]))
             {
-                printf("%c", (s[i] - 'A' + toupper(k[j]) - 'A') % 26 + 'A');
+                printf("%c", (s[i] - 'A' + toupper(k[n]) - 'A') % 26 + 'A');
             }
-            j = (j + 1) % h;
+            n = (n + 1) % h;
         }
         // non-alpha char
         else 
         {
             printf("%c", s[i]);
-        } 
+        }                     
     }
     printf("\n");    
 }
