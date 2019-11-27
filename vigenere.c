292297903de801713de4a9483925027e5cce7cd4
mattshirvan <mattshirvan@users.noreply.github.com>
2019-11-27 20:03:33 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- vigenere.c
+++ vigenere.c
@@ -11,33 +11,39 @@ int main(int argc, string argv[])
     // check if argc is only 2
     if (argc != 2)
     {
-        // print error
         printf("Usage: ./vigenere keyword\n");
         return 1;
     }
 
     // store key
     string k = argv[1];
-    int h = strlen(k);
+    int h = strlen(argv[1]);
     
     // iterate over argc for arguments
-    for (int i = 0; i < h ; i++)
+    for (int i = 0; i < argc; i++)
     {
-        // validate command line is alpha
-        if (!isalpha(k[i]))
+        // iterate over command line argument to find char in string
+        for (int j = 0, n = strlen(argv[i]); j < n; j++)
         {
-            // print error
-            printf("Usage: ./vigenere keyword\n");
-            return 1;
+            // iterate over command line argv1 string
+            for (int key = 0; key < h; key++)
+            {
+                // validate command line is not alpha
+                if (!isalpha(argv[1][key]) || k < 0)
+                {
+                    printf("Usage: ./vigenere keyword\n");
+                    return 1;
+                }
+            }
         }
     }
-   
+    
     // prompt user for string
     string s = get_string("plaintext: ");
     printf("ciphertext: ");
 
     // iterate over plain text
-    for (int i = 0, n = 0, j = strlen(s); i < j; i++)
+    for (int i = 0, j = 0, n = strlen(s); i < n; i++)
     {
         // keep uppercase
         if (isalpha(s[i]))
@@ -45,21 +51,21 @@ int main(int argc, string argv[])
             // cipher only alpha characters
             if (islower(s[i]))
             {
-                printf("%c", (s[i] - 'a' + toupper(k[n]) - 'A') % 26 + 'a');
+                printf("%c", (s[i] - 'a' + toupper(k[j]) - 'A') % 26 + 'a');
             }
             
             // keep uppercase
             else if (isupper(s[i]))
             {
-                printf("%c", (s[i] - 'A' + toupper(k[n]) - 'A') % 26 + 'A');
+                printf("%c", (s[i] - 'A' + toupper(k[j]) - 'A') % 26 + 'A');
             }
-            n = (n + 1) % h;
+            j = (j + 1) % h;
         }
         // non-alpha char
         else 
         {
             printf("%c", s[i]);
-        }                     
+        } 
     }
     printf("\n");    
 }
