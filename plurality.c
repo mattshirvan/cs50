7a2cf85ee1611fd131008361ff182ac65ca09f43
mattshirvan <mattshirvan@users.noreply.github.com>
2020-01-19 19:51:12 +0000

    automated commit by check50 [check50=True]

--- plurality.c
+++ plurality.c
@@ -67,7 +67,7 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
-    for (int i = 0; i < MAX; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
